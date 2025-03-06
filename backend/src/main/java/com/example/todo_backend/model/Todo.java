893f79dd276ea31aac2833ee176ece2cc8141f12
uetyama1173 <show_0310k@yahoo.co.jp>
2025-03-06 20:34:54 +0900

    chore: JUnit依存関係の追加　& test: 単体テストコードの実装

--- backend/src/main/java/com/example/todo_backend/model/Todo.java
+++ backend/src/main/java/com/example/todo_backend/model/Todo.java
@@ -13,7 +13,6 @@ public class Todo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String title;
     private boolean completed;
 }
--- backend/src/test/java/com/example/todo_backend/TodoBackendApplicationTests.java
+++ backend/src/test/java/com/example/todo_backend/TodoBackendApplicationTests.java
@@ -1,14 +1,35 @@
 package com.example.todo_backend;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+
 @SpringBootTest
+@AutoConfigureMockMvc
 @ActiveProfiles("test")
 class TodoBackendApplicationTests {
 
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void testGetTodos() throws Exception {
+
+        mockMvc.perform(get("/todos/read"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("[]"));
     }
 }
