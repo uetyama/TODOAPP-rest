322cee50452f072823e20932199f91c0918a5521
uetyama1173 <show_0310k@yahoo.co.jp>
2025-03-06 20:55:59 +0900

    chore:

--- backend/src/test/java/com/example/todo_backend/TodoBackendApplicationTests.java
+++ backend/src/test/java/com/example/todo_backend/TodoBackendApplicationTests.java
@@ -6,7 +6,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
